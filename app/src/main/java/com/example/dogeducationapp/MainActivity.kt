2c270b54863c7e2fced382a00d4e992d30081248
marek94 <pawluch.marek@gmail.com>
2023-10-26 20:34:50 +0200

    Step_1 add second activity and navigate to it after clicking on button

--- app/src/main/java/com/example/dogeducationapp/MainActivity.kt
+++ app/src/main/java/com/example/dogeducationapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.dogeducationapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nextButton = findViewById<Button>(R.id.next_activity)
+
+        nextButton.setOnClickListener {
+            // use explicit intent to start new activity
+            val intent = Intent(this, SecondActivity::class.java)
+
+            // start activity with a newly created intent
+            startActivity(intent)
+        }
     }
 }
