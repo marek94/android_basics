34b892b1153195cb64055002dc145a7dc9993dc9
marek94 <pawluch.marek@gmail.com>
2023-10-30 06:51:37 +0100

    Step_3 passing data between fragments (https://developer.android.com/guide/fragments/communicate)

--- app/src/main/java/com/example/dogeducationapp/Fragment1.kt
+++ app/src/main/java/com/example/dogeducationapp/Fragment1.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
 import com.example.dogeducationapp.databinding.Fragment1Binding
 
@@ -26,6 +28,14 @@ class Fragment1 : Fragment() {
 
         binding.next.setOnClickListener {
             findNavController().navigate(Fragment1Directions.actionFragment1ToFragment2())
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setFragmentResultListener("CONTRACT_KEY_USED_BY_ANDROID_TO_BIND_COMMUNICATION_BETWEEN_FRAGMENTS") { requestKey, bundle ->
+            Toast.makeText(this.requireActivity(), "Result is ${bundle.getString("result", "")}", Toast.LENGTH_LONG).show()
         }
     }
 }
--- app/src/main/java/com/example/dogeducationapp/Fragment2.kt
+++ app/src/main/java/com/example/dogeducationapp/Fragment2.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
+import androidx.navigation.fragment.findNavController
 import com.example.dogeducationapp.databinding.Fragment2Binding
 
 class Fragment2 : Fragment() {
@@ -19,5 +22,15 @@ class Fragment2 : Fragment() {
         return Fragment2Binding.inflate(inflater, container, false).apply {
             binding = this
         }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.save.setOnClickListener {
+            val result = binding.input.text.toString()
+            setFragmentResult("CONTRACT_KEY_USED_BY_ANDROID_TO_BIND_COMMUNICATION_BETWEEN_FRAGMENTS", bundleOf("result" to result))
+            findNavController().navigateUp()
+        }
     }
 }
